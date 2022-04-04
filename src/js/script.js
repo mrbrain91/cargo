d0725386770e0b35f470f5cdba37a7001d8b89fd
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-04-04 17:06:26 +0500

    start cargo web site

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//     $('').slick({
+//         infinite: true,
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         dots: true
+//     });
+// });
