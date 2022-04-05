7f46ab655ebb3b0a2f1925ba0dafe388d21e7657
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-04-05 15:42:54 +0500

    added slide banner

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
-// $(document).ready(function() {
-//     $('').slick({
-//         infinite: true,
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         dots: true
-//     });
-// });
+$(document).ready(function() {
+    $('.ban__carousel-inner').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+});
